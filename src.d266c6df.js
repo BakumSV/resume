a594411c8bb92837e2f385f99a194882f250d466
BakumSV <immigrantsong61@gmail.com>
2021-10-06 14:45:24 +0300

    Updates

--- src.d266c6df.js
+++ src.d266c6df.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.css");
 },{"./styles.css":"Tnu0"}]},{},["Focm"], null)
-//# sourceMappingURL=/resume/src.a8328bb2.js.map
+//# sourceMappingURL=/resume/src.d266c6df.js.map
